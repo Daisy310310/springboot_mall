fde9652a7de79db4c9120a138df49a09d4a2e012
Daisy310310 <egroup.intern.daisy@gmail.com>
2023-08-06 04:44:50 +0800

    商品功能－實作刪除商品

--- springboot-mall_2/src/main/java/com/yunchen/springbootmall_2/controller/ProductController.java
+++ springboot-mall_2/src/main/java/com/yunchen/springbootmall_2/controller/ProductController.java
@@ -55,4 +55,12 @@ public class ProductController {
 
         return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
     }
+
+    //刪除商品
+    @DeleteMapping("/products/{productId}")
+    public  ResponseEntity<?> deleteProduct(@PathVariable Integer productId){
+        productService.deleteProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }

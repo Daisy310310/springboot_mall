2645d4fafd7ac3d424a5863b09c18d57233ef97f
Daisy310310 <egroup.intern.daisy@gmail.com>
2023-08-06 13:16:48 +0800

    商品功能－實作查詢商品列表(分頁)

--- springboot-mall_2/src/main/java/com/yunchen/springbootmall_2/dao/impl/ProductDaoImpl.java
+++ springboot-mall_2/src/main/java/com/yunchen/springbootmall_2/dao/impl/ProductDaoImpl.java
@@ -32,6 +32,7 @@ public class ProductDaoImpl implements ProductDao {
 
         Map<String, Object> map = new HashMap<>();
 
+        //查詢條件
         if (productQueryParams.getCategory() != null){
             sql = sql + " AND category = :category"; //AND前的空白鍵很重要!!才不回和前面查詢語法黏在一起
             map.put("category", productQueryParams.getCategory().name()); //因為category是enum類型，故須使用.name()將其轉換為字串
@@ -42,7 +43,13 @@ public class ProductDaoImpl implements ProductDao {
             map.put("search", "%" + productQueryParams.getSearch() +  "%");
         }
 
+        //排序
         sql = sql + " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
+
+        //分頁
+        sql = sql + " LIMIT :limit OFFSET :offset";
+        map.put("limit", productQueryParams.getLimit());
+        map.put("offset", productQueryParams.getOffset());
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
         return productList;

a2ddc0c5ab69c167eb27948c5a374ab99af16082
Daisy310310 <egroup.intern.daisy@gmail.com>
2023-08-06 14:57:55 +0800

    商品功能－提煉程式

--- springboot-mall_2/src/main/java/com/yunchen/springbootmall_2/dao/impl/ProductDaoImpl.java
+++ springboot-mall_2/src/main/java/com/yunchen/springbootmall_2/dao/impl/ProductDaoImpl.java
@@ -30,15 +30,7 @@ public class ProductDaoImpl implements ProductDao {
         Map<String, Object> map = new HashMap<>();
 
         //查詢條件
-        if (productQueryParams.getCategory() != null){
-            sql = sql + " AND category = :category"; //AND前的空白鍵很重要!!才不回和前面查詢語法黏在一起
-            map.put("category", productQueryParams.getCategory().name()); //因為category是enum類型，故須使用.name()將其轉換為字串
-        }
-
-        if (productQueryParams.getSearch() != null){
-            sql = sql + " AND product_name LIKE :search";
-            map.put("search", "%" + productQueryParams.getSearch() +  "%");
-        }
+        sql = addFilteringSql(sql, map, productQueryParams);
 
         Integer total = namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
 
@@ -55,15 +47,7 @@ public class ProductDaoImpl implements ProductDao {
         Map<String, Object> map = new HashMap<>();
 
         //查詢條件
-        if (productQueryParams.getCategory() != null){
-            sql = sql + " AND category = :category"; //AND前的空白鍵很重要!!才不回和前面查詢語法黏在一起
-            map.put("category", productQueryParams.getCategory().name()); //因為category是enum類型，故須使用.name()將其轉換為字串
-        }
-
-        if (productQueryParams.getSearch() != null){
-            sql = sql + " AND product_name LIKE :search";
-            map.put("search", "%" + productQueryParams.getSearch() +  "%");
-        }
+        sql = addFilteringSql(sql, map, productQueryParams);
 
         //排序
         sql = sql + " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
@@ -153,6 +137,20 @@ public class ProductDaoImpl implements ProductDao {
         map.put("productId", productId);
 
         namedParameterJdbcTemplate.update(sql, map);
+    }
+
+    private  String addFilteringSql(String sql, Map<String, Object> map, ProductQueryParams productQueryParams) {
+        if (productQueryParams.getCategory() != null) {
+            sql = sql + " AND category = :category"; //AND前的空白鍵很重要!!才不回和前面查詢語法黏在一起
+            map.put("category", productQueryParams.getCategory().name()); //因為category是enum類型，故須使用.name()將其轉換為字串
+        }
+
+        if (productQueryParams.getSearch() != null) {
+            sql = sql + " AND product_name LIKE :search";
+            map.put("search", "%" + productQueryParams.getSearch() + "%");
+        }
+
+        return sql;
     }
 }
 
